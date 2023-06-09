279b58d192de8ea04c6088cf1e5fc8a21555cb07
Antonio <catachthonius01@gmail.com>
2023-06-09 15:03:50 +0200

    First commit

---
+++ src/IfElseStatement/IfElseStatement.java
@@ -0,0 +1,13 @@
+package IfElseStatement;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        String myname = "Antonio";
+
+        if(myname.length() % 2 == 0){
+            System.out.println("My name's letters number is even.");
+        } else {
+            System.out.println("My name's letters number is odd.");
+        }
+    }
+}
